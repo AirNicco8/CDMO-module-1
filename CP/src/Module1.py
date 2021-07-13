97fc7e91987d7ff427e3ca27177207d4b236a8e0
Treyvian <davide.pozzoli@gmail.com>
2021-07-13 19:16:44 +0200

    CP improvment

--- CP/src/Module1.py
+++ CP/src/Module1.py
@@ -1,9 +1,12 @@
+from minizinc import Instance, Model, Solver
+
+from datetime import timedelta
 import fileinput
 import matplotlib.pyplot as plt
-from minizinc import Instance, Model, Solver
 import os
+import time
 
-num = 1
+num = 11
 
 #reading the instance
 def read_instance(i):
@@ -34,7 +37,7 @@ def plot_solution(width, n_rets, sizes, positions):
 
     fig, ax = plt.subplots()
 
-    colors = ['tab:blue','tab:orange', 'tab:green', 'tab:red','tab:grey','tab:purple','tab:brown']
+    colors = ['tab:blue','tab:orange', 'tab:green', 'tab:red','tab:grey','tab:purple','tab:brown', 'black', 'yellow', 'gold']
     for i in range(n_rets):
         ax.broken_barh([(positions[i][0], sizes[i][0])], (positions[i][1], sizes[i][1]), facecolors=colors[i%len(colors)],edgecolors=("black",),linewidths=(1,),)
 
@@ -59,6 +62,10 @@ for i in range(n_rets):
     for j in range(2):
         sizes[i][j] = int(sizes[i][j])
 
+#ordering rectangles based on their area
+#area = [i[0]*i[1] for i in sizes]
+#ssizes = [x for _, x in sorted(zip(area, sizes), reverse=True)]
+
 #load model from file
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, "./CP_base.mzn")
@@ -73,10 +80,15 @@ instance = Instance(gecode, model)
 #Assignment
 instance["width"] = width
 instance["n_rets"]= n_rets
-instance["sizes"] =  sizes
+instance["sizes"] = sizes
+
+print(sizes)
 
 
-result = instance.solve()
+start = time.time()
+result = instance.solve(timeout=timedelta(seconds=300), processes=10)
+end = time.time()
+print("Time elapsed: {} seconds".format(end - start))
 
 height = result['objective']
 positions = result['positions']
