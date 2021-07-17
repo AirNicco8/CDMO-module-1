1e62c04b3b07a540721602a013888a5db053eafd
Treyvian <davide.pozzoli@gmail.com>
2021-07-17 18:13:53 +0200

    sat e CP

--- SAT/src/sat.py
+++ SAT/src/sat.py
@@ -1,4 +1,5 @@
 from z3 import *
+import matplotlib.pyplot as plt
 from itertools import combinations
 
 
@@ -19,6 +20,26 @@ def read_instance(i):
         
 s = read_instance(num)
 
+# Method used to plot the solution
+def plot_solution(width, n_rets, sizes, positions, height):
+    print("Sizes: ",sizes)
+    print("Positions: ", positions)
+
+    fig, ax = plt.subplots()
+
+    colors = ['tab:blue','tab:orange', 'tab:green', 'tab:red','tab:grey','tab:purple','tab:brown', 'black', 'yellow', 'gold']
+    for i in range(n_rets):
+        ax.broken_barh([(positions[i][0], sizes[i][0])], (positions[i][1], sizes[i][1]), facecolors=colors[i%len(colors)],edgecolors=("black",),linewidths=(1,),)
+
+    ax.set_ylim(0, height)
+    ax.set_xlim(0, width)
+    ax.set_xticks(range(width+1))
+    ax.set_yticks(range(height+1))
+    ax.set_xlabel('width')
+    ax.set_ylabel('height')
+
+    plt.show()
+
 # Formatting the file
 width = int(s[0])
 n_rets = int(s[1])
@@ -32,15 +53,6 @@ max_height = 0
 for i in range(n_rets):
     max_height = max_height + sizes[i][1]
 
-# Calculation of the area
-# area = [sizes[i][0]*sizes[i][1] for i in range(n_rets)]
-# space_occupied = sum(area)
-# free_space = (max_height*width) - space_occupied
-# area.append(free_space)
-# print(area)
-rangesx, rangesy = [], []
-rangesx = [(width-sizes[i][0]+1) for n in range(n_rets)]
-ragnesy = [(width-sizes[i][1]+1) for n in range(n_rets)]
 ###############################SAT MODEL########################################
 
 # Functions
@@ -54,67 +66,50 @@ def exactly_one(solver, bool_vars):
     solver.add(at_most_one(bool_vars))
     solver.add(at_least_one(bool_vars))
 
-# def exactly_n(literals, n):
-#     c = []
-#     for pairs in combinations(literals, n+1):
-#         c += [Not(And(pairs))]
-#     # At least
-#     for pairs in combinations(literals, n):
-#         c += [Or(pairs)]
-#     return And(c)
-
-
-#variables
-px = [[Bool(f"x_{n}_{i}") for i in range(len(rangesx))] for n in range(n_rets)]
-py = [[Bool(f"y_{n}_{i}") for i in range(len(rangesy))] for n in range(n_rets)]
+# Variables
+p = [[[Bool(f"x_{i}_{j}_{n}") for n in range(n_rets+1)] for j in range(max_height)] for i in range(width)]
 
 s = Solver()
 
-# Order Constraint 
-# For the x
-for i in range(n_rets):
-    ord = []
-    for j in range(len(rangesx)-1):
-        ord += [Or(Not(px[i][j]), px[i][j+1])]
-    s.add(ord)
-
-# For the y
-for i in range(n_rets):
-    ordi = []
-    for j in range(len(rangesy)-1):
-        ordi += [Or(Not(py[i][j]), py[i][j+1])]
-    s.add(ordi)
-
 # A cell has only one value
-# for i in range(width):
-#     for j in range(max_height):
-#         exactly_one(s, p[i][j])
+for i in range(width):
+    for j in range(max_height):
+        exactly_one(s, p[i][j])
 
-# there can be only a number of variables true for each rectangle
-# This number is equal to the area of each rectangle
-# for k in range(totk):
-#     g = []
-#     for i in range(width):
-#         for j in range(max_height):
-#             g += [p[i][j][k]]
-#     s.add(exactly_n(g, area[k]))
+# A rectangle has only one position
+for n in range(n_rets):
+    exactly_one(s, [p[i][j][n] for i in range(width) for j in range(max_height)])
 
-# sol = []
-# if s.check() == sat:
-#     m = s.model()
-#     for i in range(width):
-#         sol.append([])
-#         for j in range(max_height):
-#             for k in range(totk):
-#                 if m.evaluate(p[i][j][k]):
-#                     sol[i].append(k)
-# else:
-#     print("Failed to solve")
+# Position should respect width
+for n in range(n_rets):
+    s.add(at_least_one([p[i][j][n] for i in range(width-sizes[n][0]+1) for j in range(max_height)]))
+
+# Position should respect height
+for n in range(n_rets):
+    s.add(at_least_one([p[i][j][n] for i in range(width) for j in range(max_height-sizes[n][1]+1)]))
+
+# Solving overlapping
+        
 
 sol = []
 if s.check() == sat:
     m = s.model()
-    print(m)
+    for i in range(width):
+        sol.append([])
+        for j in range(max_height):
+            for k in range(n_rets+1):
+                if m.evaluate(p[i][j][k]):
+                    sol[i].append(k)
 else:
-    print("Failed to solve")
+     print("Failed to solve")
+
+positions = []
+for i in range(len(sol)):
+    for j in range(len(sol[0])):
+        if sol[i][j] != n_rets:
+            positions.append([i,j])
+
+plot_solution(width, n_rets, sizes, positions, max_height)
+
+
 
